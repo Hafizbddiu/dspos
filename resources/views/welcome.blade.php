5820958f6f68ef37b25895d50484bd9d3a2e73d7
iftakharul-islam <iftakharulislam881@gmail.com>
2022-05-29 12:03:14 -0700

    home page html structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,29 @@
     <title>Document</title>
 </head>
 <body>
-    safdfasdf
+   <header>
+       <div class="top-nav"></div>
+       <nav>
+           <div class="ds-logo-area">
+               <a href="#">
+                   <img src="logo.png" alt="logo" class="ds-logo">
+               </a>
+           </div>
+           <ul>
+               <li><a href="#">Home</a></li>
+           </ul>
+       </nav>
+   </header>
+   <main>
+       <h2>DsPos</h2>
+       <section>
+           <div class="bredcrumb">DsPos > Home </div>
+       </section>
+   </main>
+   <footer>
+       <div>
+           <p>Developed By <a href="#" class="link">Laravine Solution</p>
+       </div>
+   </footer>
 </body>
 </html>
